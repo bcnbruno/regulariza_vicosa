aa0e843ad1c08e100496c0c7dd482f9f91fc33d6
Bruno Nascimento <b_cnbruno@hotmail.com>
2019-01-29 02:17:48 -0200

    Correção Filtro

---
+++ ambiental/dados/static/js/rango-jquery.js
@@ -0,0 +1,24 @@
+$(document).ready( function() {
+    $("#id_bairro").change( function(event) {
+        var bairro = $(this).val();
+        
+        $.ajax({
+            url: '/ajax/escolha_bairro/',
+            data: {
+                'bairro': bairro
+            },
+            success: function (data) {
+                ruas = data.nomes.split(","); 
+                ruas.sort();
+                str = '';                
+                for( i=0; i<ruas.length; i++ ){
+                    str += "<option value=" + ruas[i] + ">" + ruas[i] + "</option>";
+                }
+                $("#id_nome").html(str);
+            },
+            dataType: 'json'
+        });
+
+    });
+});
+
